823b7336391c5da1d5a95960bd3c93c76b5d7a0b
Aman <aman.kumar99499@gmail.com>
2022-08-15 14:53:20 +0530

    ADDED : Changing-or-editing-details

--- src/com/AddressBook/AddressBook.java
+++ src/com/AddressBook/AddressBook.java
@@ -13,7 +13,8 @@ public class AddressBook {
         Scanner scan = new Scanner(System.in);
         while (true) {
             System.out.println("Choose option which you want to Perform");
-            System.out.println("Press 1 for Creating New Address / Press 2 to View the Address");
+            System.out.println("Press 1 for Creating New Address \nPress 2 to View All the Address"
+            + "\nPress 3 to Edit the Existing address");
             int option = Integer.parseInt(scan.nextLine());
             switch (option) {
                 case 1:
@@ -50,7 +51,54 @@ public class AddressBook {
                         System.out.println(address1);
                     }
                     System.out.println("\n\n");
-                default:
+                    break;
+                case 3:
+                    System.out.println("Editing Records");
+                    System.out.println("Enter the First Name which you want to Edit");
+                    for(int i=0;i<list.size();i++){
+                        address1=list.get(i);
+                        if(scan.nextLine().equals(address1.getFirstName())){
+                            System.out.println("Press 1 to Change first Name");
+                            System.out.println("Press 2 to Change Name");
+                            System.out.println("Press 3 to Change City ");
+                            System.out.println("Press 4 to Change State ");
+                            System.out.println("Press 5 to Change Zip ");
+                            System.out.println("Press 6 to Change Email ");
+                            System.out.println("Press 7 to Change Phone Number");
+                            int option2 = scan.nextInt();
+                            scan.nextLine();
+                            switch (option2) {
+                                case 1:
+                                    System.out.println("Enter Your First Name");
+                                    String FirstName = scan.nextLine();
+                                    address1.setFirstName(FirstName);
+                                case 2:
+                                    System.out.println("Enter Your Last Name");
+                                    address1.setLastName(scan.nextLine());
+                                case 3:
+                                    System.out.println("Enter Your City or Location");
+                                    address1.getCity(scan.nextLine());
+                                case 4:
+                                    System.out.println("Enter Your State");
+                                    address1.getState(scan.nextLine());
+                                case 5:
+                                    System.out.println("Enter Your Zip Code");
+                                    address1.setZip(scan.nextLine());
+                                case 6:
+                                    System.out.println("Enter Your Email Address");
+                                    address1.setEmail(scan.nextLine());
+                                case 7:
+                                    System.out.println("Enter Your Phone Number");
+                                    address1.setPhoneNumber(scan.nextLine());
+                                    //
+                                    list.add(address1);
+                            }
+                        }
+                        else{
+                            System.out.println("Details Not Found");
+                            }
+                    }
+                    default:
                     System.out.println("Enter correct option");
             }
             System.out.println("Details are " + list);
